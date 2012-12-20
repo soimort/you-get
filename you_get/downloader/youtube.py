ccdeeadc4918c1882815b3272ad226cc8d8bc495
Mort Yao <mort.yao@gmail.com>
2012-12-20 03:30:52 +0100

    YouTube: quick dirty fix to #45

--- you_get/downloader/youtube.py
+++ you_get/downloader/youtube.py
@@ -30,12 +30,19 @@ def youtube_download_by_id(id, title = None, output_dir = '.', merge = True, inf
         '17',
         '13',
     ]:
-        fmt = r1(r'itag=' + itag + r'\\u0026([^,]+),', html)
+        fmt = r1(r'url=([^,]+)\\u0026itag=' + itag + ",", html)
         if fmt:
-            url = r1(r'url=([^\\]+)\\u0026', fmt)
+            url = r1(r'([^\\]+)\\u0026', fmt)
+            if not url:
+                continue
+            
             url = unicodize(url)
             url = parse.unquote(url)
+            
             sig = r1(r'sig=([^\\]+)\\u0026', fmt)
+            if not sig:
+                continue
+            
             url = url + '&signature=' + sig
             break
     try:
--- you_get/version.py
+++ you_get/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
-__version__ = '0.3dev-20121219'
-__date__ = '2012-12-19'
+__version__ = '0.3dev-20121220'
+__date__ = '2012-12-20'
