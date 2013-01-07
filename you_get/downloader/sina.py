8ef2e0de8bbc826224d30380c0ed2177733803e9
Mort Yao <mort.yao@gmail.com>
2013-01-07 23:39:45 +0100

    Sina: fix regression issue introduced by 8d0c066

--- you_get/downloader/sina.py
+++ you_get/downloader/sina.py
@@ -27,7 +27,9 @@ def sina_download_by_id(id, title = None, output_dir = '.', merge = True, info_o
         download_urls(urls, title, 'flv', size, output_dir = output_dir, merge = merge)
 
 def sina_download(url, output_dir = '.', merge = True, info_only = False):
-    id = r1(r'vid\s*:\s*\'(\d+)\',\n', get_html(url))
+    id = r1(r'[^_]vid\s*:\s*\'([^\']+)\',', get_html(url)).split('|')[-1]
+    assert id
+    
     sina_download_by_id(id, output_dir = output_dir, merge = merge, info_only = info_only)
 
 site_info = "Sina.com"
