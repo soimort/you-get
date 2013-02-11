4c5a2540ea665d763e7a66fcae108dd1a2656a00
Mort Yao <mort.yao@gmail.com>
2013-02-11 20:07:42 +0100

    Mixcloud: fix file extension issue

--- you_get/downloader/mixcloud.py
+++ you_get/downloader/mixcloud.py
@@ -21,7 +21,7 @@ def mixcloud_download(url, output_dir = '.', merge = True, info_only = False):
     
     print_info(site_info, title, type, size)
     if not info_only:
-        download_urls([url], title, type, size, output_dir, merge = merge)
+        download_urls([url], title, ext, size, output_dir, merge = merge)
 
 site_info = "Mixcloud.com"
 download = mixcloud_download
