c8508ca6bfdbb4df58e30b9cbed5d5014bd2f30f
Mort Yao <mort.yao@gmail.com>
2013-06-07 00:32:00 +0200

    Sina: fix #186

--- src/you_get/downloader/sina.py
+++ src/you_get/downloader/sina.py
@@ -27,7 +27,7 @@ def sina_download_by_id(id, title = None, output_dir = '.', merge = True, info_o
         download_urls(urls, title, 'flv', size, output_dir = output_dir, merge = merge)
 
 def sina_download(url, output_dir = '.', merge = True, info_only = False):
-    id = r1(r'[^_]vid\s*:\s*\'([^\']+)\',', get_html(url)).split('|')[-1]
+    id = r1(r'vid=(\d+)', url) or r1(r'[^_]vid\s*:\s*\'([^\']+)\',', get_html(url)).split('|')[-1]
     assert id
     
     sina_download_by_id(id, output_dir = output_dir, merge = merge, info_only = info_only)
