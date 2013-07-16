02e1d3fe81b532eada39101465de1e4ab0542001
Mort Yao <mort.yao@gmail.com>
2013-07-16 22:57:23 +0800

    bilibili: fix #209

--- src/you_get/downloader/bilibili.py
+++ src/you_get/downloader/bilibili.py
@@ -83,7 +83,7 @@ def bilibili_download(url, output_dir = '.', merge = True, info_only = False):
     title = unescape_html(title)
     title = escape_file_path(title)
     
-    flashvars = r1_of([r'flashvars="([^"]+)"', r'"https://secure.bilibili.tv/secure,(cid=\d+)(?:&aid=\d+)?"'], html)
+    flashvars = r1_of([r'player_params=\'(cid=\d+)', r'flashvars="([^"]+)"', r'"https://secure.bilibili.tv/secure,(cid=\d+)(?:&aid=\d+)?"'], html)
     assert flashvars
     t, id = flashvars.split('=', 1)
     id = id.split('&')[0]
