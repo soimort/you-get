89caa854f4d976ba5af6875ab0bc1f74db6555e8
David Parunakian <jaffar.rumith@gmail.com>
2013-08-08 21:51:52 +0400

    Fixed a bug: no parameters were getting passed to the youtube_download_by_id() function

--- src/you_get/downloader/khan.py
+++ src/you_get/downloader/khan.py
@@ -8,7 +8,7 @@ from .youtube import youtube_download_by_id
 def khan_download(url, output_dir = '.', merge = True, info_only = False):
     page = get_html(url)
     id = page[page.find('src="https://www.youtube.com/embed/') + len('src="https://www.youtube.com/embed/') :page.find('?enablejsapi=1&wmode=transparent&modestbranding=1&rel=0&fs=1&showinfo=0')]
-    youtube_download_by_id(id)
+    youtube_download_by_id(id, output_dir=output_dir, merge=merge, info_only=info_only)
 
 site_info = "khanacademy.org"
 download = khan_download
