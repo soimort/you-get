610365a37ca3a3f46cd6da67fa4a110e317513cb
Mort Yao <mort.yao@gmail.com>
2013-09-11 13:50:39 +0200

    cli_wrapper

--- src/you_get/__init__.py
+++ src/you_get/__init__.py
@@ -3,8 +3,5 @@
 from .common import *
 from .version import *
 
-# Easy import
-#from .cli_wrapper.converter import *
-#from .cli_wrapper.player import *
 from .cli_wrapper import *
 from .extractor import *
