869128c8c7f1a8d4de0f592ded338d0833f8d453
Mort Yao <mort.yao@gmail.com>
2014-02-14 09:14:04 +0100

    Niconico: workaround for TLS hang bug, fix #296

--- src/you_get/extractor/nicovideo.py
+++ src/you_get/extractor/nicovideo.py
@@ -6,12 +6,17 @@ from ..common import *
 
 def nicovideo_login(user, password):
     data = "current_form=login&mail=" + user +"&password=" + password + "&login_submit=Log+In"
-    response = request.urlopen(request.Request("https://secure.nicovideo.jp/secure/login?site=niconico", headers = fake_headers, data = data.encode('utf-8')))
+    response = request.urlopen(request.Request("https://secure.nicovideo.jp/secure/login?site=niconico", headers=fake_headers, data=data.encode('utf-8')))
     return response.headers
 
-def nicovideo_download(url, output_dir = '.', merge = True, info_only = False):
-    request.install_opener(request.build_opener(request.HTTPCookieProcessor()))
-    
+def nicovideo_download(url, output_dir='.', merge=True, info_only=False):
+    import ssl
+    ssl_context = request.HTTPSHandler(
+context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
+    cookie_handler = request.HTTPCookieProcessor()
+    opener = request.build_opener(ssl_context, cookie_handler)
+    request.install_opener(opener)
+
     import netrc, getpass
     info = netrc.netrc().authenticators('nicovideo')
     if info is None:
@@ -21,15 +26,15 @@ def nicovideo_download(url, output_dir = '.', merge = True, info_only = False):
         user, password = info[0], info[2]
     print("Logging in...")
     nicovideo_login(user, password)
-    
+
     html = get_html(url) # necessary!
     title = unicodize(r1(r'<span class="videoHeaderTitle">([^<]+)</span>', html))
-    
+
     api_html = get_html('http://www.nicovideo.jp/api/getflv?v=%s' % url.split('/')[-1])
     real_url = parse.unquote(r1(r'url=([^&]+)&', api_html))
-    
+
     type, ext, size = url_info(real_url)
-    
+
     print_info(site_info, title, type, size)
     if not info_only:
         download_urls([real_url], title, ext, size, output_dir, merge = merge)
