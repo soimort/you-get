063cc9932c290e766a49487dffef3bcda894df2d
yoke88 <yoke-msn@hotmail.com>
2014-06-05 21:29:12 +0800

    Update sohu.py

--- src/you_get/extractor/sohu.py
+++ src/you_get/extractor/sohu.py
@@ -7,9 +7,23 @@ from ..common import *
 import json
 
 def real_url(host, prot, file, new):
+    
+    #http://211.162.61.44/sohu/s26h23eab6/10/|377|115.174.81.252|isvS4ypzVNTehZucPYiuvSofPLJKZjjRfj-89w..|1|0|13|1803|1
+    #http://220.181.61.212/?prot=2&file=/tv/HUGE/BackUp/hadoop/20140129/1134771_1575912_tv_S_030057_3101/1134771_1575912_tv_S_030057_3101_006.mp4&new=/95/134/sPDUFfcxumz9E19hrO5kF1.mp4&idc=377&key=P8F1ZUA9aCmlVvSwtSGLuf95W_fuRY69&vid=1575912&uid=14016791428538333287&sz=1663_538&md=exxLvREv9OqzTmsj6bmTAFSZZKl6wBx6OgUYqQ==206&t=0.7982267350889742
+    i=0
     url = 'http://%s/?prot=%s&file=%s&new=%s' % (host, prot, file, new)
-    start, _, host, key = get_html(url).split('|')[:4]
-    return '%s%s?key=%s' % (start[:-1], new, key)
+    while i<=5:
+        try:
+            start, idc, host2, key = get_html(url).split('|')[:4]
+            realurl='%s%s?key=%s' % (start[:-1], new, key)
+            response1=request.urlopen(realurl)
+            return realurl
+        except:
+            url='http://%s/?prot=%s&file=%s&new=%s&idc=%s' % (host, prot, file, new,idc)
+            #print(host)
+            #print(url)
+            start, idc, host2, key = get_html(url).split('|')[:4]
+            i=i+1
 
 def sohu_download(url, output_dir = '.', merge = True, info_only = False):
     if re.match(r'http://share.vrs.sohu.com', url):
