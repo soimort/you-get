ca5151ee58f84715850cd90b5c0a431bcc109452
jackyzy823 <jackyzy823@gmail.com>
2014-07-10 22:13:49 +0800

    add not implement error for kugou

--- src/you_get/extractor/kugou.py
+++ src/you_get/extractor/kugou.py
@@ -9,6 +9,7 @@ from base64 import b64decode
 
 def kugou_download(url, output_dir=".", merge=True, info_only=False):
     if url.lower().find("5sing")!=-1:
+        #for 5sing.kugou.com
         html=get_html(url)
         ticket=r1(r'"ticket":\s*"(.*)"',html)
         j=loads(str(b64decode(ticket),encoding="utf-8"))
@@ -18,10 +19,9 @@ def kugou_download(url, output_dir=".", merge=True, info_only=False):
         print_info(site_info, title, songtype, size)
         if not info_only:
             download_urls([url], title, ext, size, output_dir)
-            
-    # html = get_html(url)
-    # title = r1(r'var SongName   = "(.*)";', html)
-    # url = r1(r'file: "(\S*)"', html)
+    else:
+        #for the www.kugou.com/
+        raise NotImplementedError(url)       
 
 site_info = "kugou.com"
 download = kugou_download
