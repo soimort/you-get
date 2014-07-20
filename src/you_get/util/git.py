e8bf96fc212225a6482fa1976ba8843e1f9a7082
Mort Yao <mort.yao@gmail.com>
2014-07-21 00:25:26 +0200

    update util.git

--- src/you_get/util/git.py
+++ src/you_get/util/git.py
@@ -4,5 +4,10 @@ import os
 
 def get_head(repo_path):
     """Get (branch, commit) from HEAD of a git repo."""
-    ref = open(os.path.join(repo_path, '.git', 'HEAD'), 'r').read().strip()[5:].split('/')
-    return ref[-1], open(os.path.join(repo_path, '.git', *ref), 'r').read().strip()[:7]
+    try:
+        ref = open(os.path.join(repo_path, '.git', 'HEAD'), 'r').read().strip()[5:].split('/')
+        branch = ref[-1]
+        commit = open(os.path.join(repo_path, '.git', *ref), 'r').read().strip()[:7]
+        return branch, commit
+    except:
+        return None
