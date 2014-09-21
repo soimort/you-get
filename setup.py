b8e0600d7835f90f32fff306e3bee24a293eed13
Mort Yao <mort.yao@gmail.com>
2014-09-21 04:21:40 +0200

    use .rst as the extension name of reStructuredText files; deprecate README.txt

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ PROJ_METADATA = '%s.json' % PROJ_NAME
 import os, json, imp
 here = os.path.abspath(os.path.dirname(__file__))
 proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
-README = open(os.path.join(here, 'README.txt'), encoding='utf-8').read()
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.txt'), encoding='utf-8').read()
+README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
 VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
 
 from setuptools import setup, find_packages
