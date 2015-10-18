6409b9e022bffa4c3fa363dc797520aea3d2563a
Mort Yao <soi@mort.ninja>
2015-10-18 03:07:02 +0200

    [baomihua] fix #501

--- src/you_get/extractors/baomihua.py
+++ src/you_get/extractors/baomihua.py
@@ -6,13 +6,13 @@ from ..common import *
 
 import urllib
 
-def baomihua_download_by_id(id, title = None, output_dir = '.', merge = True, info_only = False, **kwargs):
+def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
     html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s' % id)
     host = r1(r'host=([^&]*)', html)
     assert host
     type = r1(r'videofiletype=([^&]*)', html)
     assert type
-    vid = r1(r'&stream_name=([0-9\/]+)&', html)
+    vid = r1(r'&stream_name=([^&]*)', html)
     assert vid
     url = "http://%s/pomoho_video/%s.%s" % (host, vid, type)
     _, ext, size = url_info(url)
@@ -20,13 +20,13 @@ def baomihua_download_by_id(id, title = None, output_dir = '.', merge = True, in
     if not info_only:
         download_urls([url], title, ext, size, output_dir, merge = merge)
 
-def baomihua_download(url, output_dir = '.', merge = True, info_only = False):
+def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
     html = get_html(url)
     title = r1(r'<title>(.*)</title>', html)
     assert title
-    id = r1(r'flvid=(\d+)', html)
+    id = r1(r'flvid\s*=\s*(\d+)', html)
     assert id
-    baomihua_download_by_id(id, title, output_dir = output_dir, merge = merge, info_only = info_only)
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
 
 site_info = "baomihua.com"
 download = baomihua_download
