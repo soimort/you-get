25cec6ea80e3e827230c544d3bd01b411f62f580
Mort Yao <soi@mort.ninja>
2015-10-20 03:50:38 +0200

    [vimeo] fix #718

--- src/you_get/extractors/vimeo.py
+++ src/you_get/extractors/vimeo.py
@@ -19,26 +19,34 @@ def vimeo_download_by_channel_id(channel_id, output_dir = '.', merge = False, in
     html = get_content('https://api.vimeo.com/channels/{channel_id}/videos?access_token={access_token}'.format(channel_id = channel_id, access_token = access_token))
     data = loads(html)
     id_list = []
-    
+
     #print(data)
     for i in data['data']:
         id_list.append(match1(i['uri'], r'/videos/(\w+)'))
-        
+
     for id in id_list:
         vimeo_download_by_id(id, None, output_dir, merge, info_only)
 
 def vimeo_download_by_id(id, title = None, output_dir = '.', merge = True, info_only = False):
-    video_page = get_content('http://player.vimeo.com/video/%s' % id, headers=fake_headers)
-    title = r1(r'<title>([^<]+)</title>', video_page)
+    html = get_content('https://vimeo.com/' + id)
+    config_url = unescape_html(r1(r'data-config-url="([^"]+)"', html))
+
+    if config_url:
+        video_page = get_content(config_url, headers=fake_headers)
+        title = r1(r'"title":"([^"]+)"', video_page)
+    else:
+        video_page = get_content('http://player.vimeo.com/video/%s' % id, headers=fake_headers)
+        title = r1(r'<title>([^<]+)</title>', video_page)
+
     info = dict(re.findall(r'"([^"]+)":\{[^{]+"url":"([^"]+)"', video_page))
     for quality in ['hd', 'sd', 'mobile']:
         if quality in info:
             url = info[quality]
             break
     assert url
-    
+
     type, ext, size = url_info(url, faker=True)
-    
+
     print_info(site_info, title, type, size)
     if not info_only:
         download_urls([url], title, ext, size, output_dir, merge = merge, faker = True)
@@ -49,7 +57,7 @@ def vimeo_download(url, output_dir = '.', merge = True, info_only = False, **kwa
     else:
         id = r1(r'http://[\w.]*vimeo.com[/\w]*/(\d+)$', url)
         assert id
-    
+
         vimeo_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
 
 site_info = "Vimeo.com"
