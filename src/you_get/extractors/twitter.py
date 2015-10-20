e9b93f2dee7e7602eb18f0b7de8c12ccc3f9e0cb
Mort Yao <soi@mort.ninja>
2015-10-20 05:20:15 +0200

    [twitter] download images

--- src/you_get/extractors/twitter.py
+++ src/you_get/extractors/twitter.py
@@ -8,7 +8,8 @@ def twitter_download(url, output_dir='.', merge=True, info_only=False, **kwargs)
     html = get_html(url)
     screen_name = r1(r'data-screen-name="([^"]*)"', html)
     item_id = r1(r'data-item-id="([^"]*)"', html)
-    title = "{} [{}]".format(screen_name, item_id)
+    page_title = "{} [{}]".format(screen_name, item_id)
+
     icards = r1(r'data-src="([^"]*)"', html)
     if icards:
         html = get_html("https://twitter.com" + icards)
@@ -16,11 +17,39 @@ def twitter_download(url, output_dir='.', merge=True, info_only=False, **kwargs)
         source = data['playlist'][0]['source']
     else:
         source = r1(r'<source video-src="([^"]*)"', html)
-    mime, ext, size = url_info(source)
 
-    print_info(site_info, title, mime, size)
-    if not info_only:
-        download_urls([source], title, ext, size, output_dir, merge=merge)
+    try: # extract video
+        mime, ext, size = url_info(source)
+
+        print_info(site_info, page_title, mime, size)
+        if not info_only:
+            download_urls([source], page_title, ext, size, output_dir, merge=merge)
+
+    except: # extract images
+        urls = re.findall(r'property="og:image"\s*content="([^"]+)"', html)
+        images = []
+        for url in urls:
+            url = ':'.join(url.split(':')[:-1]) + ':orig'
+            filename = parse.unquote(url.split('/')[-1])
+            title = '.'.join(filename.split('.')[:-1])
+            ext = url.split(':')[-2].split('.')[-1]
+            size = int(get_head(url)['Content-Length'])
+            images.append({'title': title,
+                           'url': url,
+                           'ext': ext,
+                           'size': size})
+        size = sum([image['size'] for image in images])
+        print_info(site_info, page_title, images[0]['ext'], size)
+
+        if not info_only:
+            for image in images:
+                title = image['title']
+                ext = image['ext']
+                size = image['size']
+                url = image['url']
+                print_info(site_info, title, ext, size)
+                download_urls([url], title, ext, size,
+                              output_dir=output_dir)
 
 site_info = "Twitter.com"
 download = twitter_download
