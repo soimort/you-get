5f339ed1fc4f46ff3116a453b47a2ea3d2512523
Mort Yao <soi@mort.ninja>
2015-10-22 14:45:57 +0200

    [veoh] set info_only=info_only

--- src/you_get/extractors/veoh.py
+++ src/you_get/extractors/veoh.py
@@ -13,17 +13,17 @@ def veoh_download(url, output_dir = '.', merge = False, info_only = False, **kwa
         item_id = match1(url, r'http://www.veoh.com/m/watch.php\?v=(\w+)')
     else:
         raise NotImplementedError('Cannot find item ID')
-    veoh_download_by_id(item_id, output_dir = '.', merge = False, info_only = False, **kwargs)
+    veoh_download_by_id(item_id, output_dir = '.', merge = False, info_only = info_only, **kwargs)
 
 #----------------------------------------------------------------------
 def veoh_download_by_id(item_id, output_dir = '.', merge = False, info_only = False, **kwargs):
     """Source: Android mobile"""
     webpage_url = 'http://www.veoh.com/m/watch.php?v={item_id}&quality=1'.format(item_id = item_id)
-    
+
     #grab download URL
     a = get_content(webpage_url, decoded=True)
     url = match1(a, r'<source src="(.*?)\"\W')
-    
+
     #grab title
     title = match1(a, r'<meta property="og:title" content="([^"]*)"')
 
