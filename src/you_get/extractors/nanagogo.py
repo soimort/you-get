fc370c522062a63d88bee5475d521f419c9e391c
Mort Yao <soi@mort.ninja>
2015-11-10 04:58:46 +0100

    [755] use movieUrlHq

--- src/you_get/extractors/nanagogo.py
+++ src/you_get/extractors/nanagogo.py
@@ -9,7 +9,7 @@ def nanagogo_download(url, output_dir='.', merge=True, info_only=False, **kwargs
     title = r1(r'<meta property="og:title" content="([^"]*)"', html)
     postId = r1(r'postId\s*:\s*"([^"]*)"', html)
     title += ' - ' + postId
-    try: # download video
+    try: # extract direct video
         source = r1(r'<meta property="og:video" content="([^"]*)"', html)
         mime, ext, size = url_info(source)
 
@@ -17,27 +17,35 @@ def nanagogo_download(url, output_dir='.', merge=True, info_only=False, **kwargs
         if not info_only:
             download_urls([source], title, ext, size, output_dir, merge=merge)
 
-    except: # download image
+    except: # official API
         talkId = r1(r'talkId\s*:\s*"([^"]*)"', html)
         apiUrl = 'http://7gogo.jp/api/talk/post/detail/%s/%s' % (talkId, postId)
         info = json.loads(get_content(apiUrl))
         images = []
         for post in info['posts']:
             for item in post['body']:
-                if 'image' not in item: continue
+                if 'movieUrlHq' in item:
+                    url = item['movieUrlHq']
+                    name = title
+                    _, ext, size = url_info(url)
+                    images.append({'title': name,
+                                   'url': url,
+                                   'ext': ext,
+                                   'size': size})
 
-                url = item['image']
-                filename = parse.unquote(url.split('/')[-1])
-                name = '.'.join(filename.split('.')[:-1])
-                ext = filename.split('.')[-1]
-                size = int(get_head(url)['Content-Length'])
-                images.append({'title': name,
-                               'url': url,
-                               'ext': ext,
-                               'size': size})
+                elif 'image' in item:
+                    url = item['image']
+                    filename = parse.unquote(url.split('/')[-1])
+                    name = '.'.join(filename.split('.')[:-1])
+                    ext = filename.split('.')[-1]
+                    size = int(get_head(url)['Content-Length'])
+                    images.append({'title': name,
+                                   'url': url,
+                                   'ext': ext,
+                                   'size': size})
 
         size = sum([i['size'] for i in images])
-        print_info(site_info, title, None, size)
+        print_info(site_info, title, ext, size)
 
         if not info_only:
             for i in images:
