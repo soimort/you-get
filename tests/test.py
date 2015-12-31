1d027d10215d9414b906737ca19a999e1a9580e1
Mort Yao <soi@mort.ninja>
2015-12-31 11:20:50 +0100

    [imgur] add test

--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,10 @@ class YouGetTests(unittest.TestCase):
     def test_freesound(self):
         freesound.download("http://www.freesound.org/people/Corsica_S/sounds/184419/", info_only=True)
 
+    def test_imgur(self):
+        imgur.download("http://imgur.com/WVLk5nD", info_only=True)
+        imgur.download("http://imgur.com/gallery/WVLk5nD", info_only=True)
+
     def test_magisto(self):
         magisto.download("http://www.magisto.com/album/video/f3x9AAQORAkfDnIFDA", info_only=True)
 
