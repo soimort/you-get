2d4ec83e4fed112809fa137150875871ba9c2bfa
ZhangShy <zaozh1990@163.com>
2016-01-12 20:07:14 +0800

    call youget.common.download_main play url

--- web_main.py
+++ web_main.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
-import subprocess
+import os, sys
 from flask import Flask, request
+
 app = Flask(__name__)
+
+_srcdir = 'src/'
+_filepath = os.path.dirname(sys.argv[0])
+sys.path.insert(1, os.path.join(_filepath, _srcdir))
+
+from you_get import common
 
 # 使用vlc进行播放
 @app.route('/play/')
@@ -10,9 +17,13 @@ def hello_world():
     url = request.args.get('url')
     if url!=None:
         print('url is::'+url)
-        cmd = 'python3 you-get -p vlc ' + url
-        subprocess.Popen(cmd, shell=True)
+        common.player = 'vlc'
+        common.download_main(common.any_download, common.any_download_playlist, [url], False,
+                             output_dir='.', merge=True, info_only=False, json_output=False, caption=True)
     return 'End'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if sys.version_info.major == 3:
+    if __name__ == '__main__':
+        app.run(debug=True)
+else:
+    print("Need python3")
