1e2f5de8454ac03d28518d74998a0fe09c9cda11
Mort Yao <soi@mort.ninja>
2016-02-07 15:13:24 +0100

    [youtube] fix #912

--- src/you_get/extractors/youtube.py
+++ src/you_get/extractors/youtube.py
@@ -86,6 +86,7 @@ class YouTube(VideoExtractor):
         return match1(url, r'youtu\.be/([^/]+)') or \
           match1(url, r'youtube\.com/embed/([^/?]+)') or \
           match1(url, r'youtube\.com/v/([^/?]+)') or \
+          match1(url, r'youtube\.com/watch/([^/?]+)') or \
           parse_query_param(url, 'v') or \
           parse_query_param(parse_query_param(url, 'u'), 'v')
 
