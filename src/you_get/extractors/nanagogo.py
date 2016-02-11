321572404b2c22dc85d3ca7b982f5b7cdfca19c1
Mort Yao <soi@mort.ninja>
2016-02-11 01:07:34 +0100

    [755] fix #919

--- src/you_get/extractors/nanagogo.py
+++ src/you_get/extractors/nanagogo.py
@@ -5,59 +5,37 @@ __all__ = ['nanagogo_download']
 from ..common import *
 
 def nanagogo_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url)
-    title = r1(r'<meta property="og:title" content="([^"]*)"', html)
-    postId = r1(r'postId\s*:\s*"([^"]*)"', html)
-    title += ' - ' + postId
-    try: # extract direct video
-        source = r1(r'<meta property="og:video" content="([^"]*)"', html)
-        mime, ext, size = url_info(source)
+    talk_id = r1(r'7gogo.jp/([^/]+)/', url)
+    post_id = r1(r'7gogo.jp/[^/]+/(\d+)', url)
+    title = '%s_%s' % (talk_id, post_id)
+    api_url = 'https://api.7gogo.jp/web/v2/talks/%s/posts/%s' % (talk_id, post_id)
+    info = json.loads(get_content(api_url))
 
-        print_info(site_info, title, mime, size)
-        if not info_only:
-            download_urls([source], title, ext, size, output_dir, merge=merge)
+    items = []
+    for i in info['data']['posts']['post']['body']:
+        if 'image' in i:
+            image_url = i['image']
+            _, ext, size = url_info(image_url)
+            items.append({'title': title,
+                          'url':   image_url,
+                          'ext':   ext,
+                          'size':  size})
+        elif 'movieUrlHq' in i:
+            movie_url = i['movieUrlHq']
+            _, ext, size = url_info(movie_url)
+            items.append({'title': title,
+                          'url':   movie_url,
+                          'ext':   ext,
+                          'size':  size})
 
-    except: # official API
-        talkId = r1(r'talkId\s*:\s*"([^"]*)"', html)
-        apiUrl = 'http://7gogo.jp/api/talk/post/detail/%s/%s' % (talkId, postId)
-        info = json.loads(get_content(apiUrl))
-        images = []
-        for post in info['posts']:
-            for item in post['body']:
-                if 'movieUrlHq' in item:
-                    url = item['movieUrlHq']
-                    name = title
-                    _, ext, size = url_info(url)
-                    images.append({'title': name,
-                                   'url': url,
-                                   'ext': ext,
-                                   'size': size})
-
-                elif 'image' in item:
-                    url = item['image']
-                    name = title
-                    #filename = parse.unquote(url.split('/')[-1])
-                    #name = '.'.join(filename.split('.')[:-1])
-                    #ext = filename.split('.')[-1]
-                    #size = int(get_head(url)['Content-Length'])
-                    _, ext, size = url_info(url)
-                    images.append({'title': name,
-                                   'url': url,
-                                   'ext': ext,
-                                   'size': size})
-
-        size = sum([i['size'] for i in images])
-        print_info(site_info, title, ext, size)
-
-        if not info_only:
-            for i in images:
-                title = i['title']
-                ext = i['ext']
-                size = i['size']
-                url = i['url']
-                print_info(site_info, title, ext, size)
-                download_urls([url], title, ext, size,
-                              output_dir=output_dir)
+    size = sum([i['size'] for i in items])
+    print_info(site_info, title, ext, size)
+    if not info_only:
+        for i in items:
+            print_info(site_info, i['title'], i['ext'], i['size'])
+            download_urls([i['url']], i['title'], i['ext'], i['size'],
+                          output_dir=output_dir,
+                          merge=merge)
 
 site_info = "7gogo.jp"
 download = nanagogo_download
