bbe1376a8670060f51803ed11d0027a3ab9792da
Mort Yao <soi@mort.ninja>
2016-07-01 15:35:02 +0200

    [showroom] support room URLs with dashes (-)

--- src/you_get/extractors/showroom.py
+++ src/you_get/extractors/showroom.py
@@ -56,8 +56,8 @@ def showroom_download_by_room_id(room_id, output_dir = '.', merge = False, info_
 #----------------------------------------------------------------------
 def showroom_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
     """"""
-    if re.match( r'(\w+)://www.showroom-live.com/(\w*)', url):
-        room_url_key = match1(url, r'\w+://www.showroom-live.com/(\w*)')
+    if re.match( r'(\w+)://www.showroom-live.com/([-\w]+)', url):
+        room_url_key = match1(url, r'\w+://www.showroom-live.com/([-\w]+)')
         room_id = showroom_get_roomid_by_room_url_key(room_url_key)
         showroom_download_by_room_id(room_id, output_dir, merge, 
                                     info_only)
