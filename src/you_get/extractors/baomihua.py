e13247f7b62157b112791ec867655476b3e4fec4
zhangchao <zhangchaolhr@qq.com>
2016-08-29 19:14:29 +0800

    qq compatible with new urls and modify baomihua getvidourl address

--- src/you_get/extractors/baomihua.py
+++ src/you_get/extractors/baomihua.py
@@ -7,7 +7,7 @@ from ..common import *
 import urllib
 
 def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s' % id)
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
     host = r1(r'host=([^&]*)', html)
     assert host
     type = r1(r'videofiletype=([^&]*)', html)
