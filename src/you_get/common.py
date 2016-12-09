253e1eb454160f83dd5a31847d4fb4ed0fd21803
SeaHOH <Seahoh@Gmail.com>
2016-12-10 03:07:12 +0800

    Just change

--- src/you_get/common.py
+++ src/you_get/common.py
@@ -1303,16 +1303,7 @@ def script_main(script_name, download, download_playlist, **kwargs):
     socket.setdefaulttimeout(timeout)
 
     try:
-        if stream_id:
-            if not extractor_proxy:
-                download_main(download, download_playlist, args, playlist, stream_id=stream_id, proxy=proxy, extractor_proxy=proxy, output_dir=output_dir, merge=merge, info_only=info_only, json_output=json_output, caption=caption)
-            else:
-                download_main(download, download_playlist, args, playlist, stream_id=stream_id, proxy=proxy, extractor_proxy=extractor_proxy, output_dir=output_dir, merge=merge, info_only=info_only, json_output=json_output, caption=caption)
-        else:
-            if not extractor_proxy:
-                download_main(download, download_playlist, args, playlist, proxy=proxy, extractor_proxy=proxy, output_dir=output_dir, merge=merge, info_only=info_only, json_output=json_output, caption=caption)
-            else:
-                download_main(download, download_playlist, args, playlist, proxy=proxy, extractor_proxy=extractor_proxy, output_dir=output_dir, merge=merge, info_only=info_only, json_output=json_output, caption=caption)
+        download_main(download, download_playlist, args, playlist, stream_id=stream_id, proxy=proxy, extractor_proxy=extractor_proxy or proxy, output_dir=output_dir, merge=merge, info_only=info_only, json_output=json_output, caption=caption)
     except KeyboardInterrupt:
         if traceback:
             raise
