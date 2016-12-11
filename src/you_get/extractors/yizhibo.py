0f1d5beb1494ca6b64b90e3d8d5949de29b2c31b
Valdemar Erk <valdemarerk@gmail.com>
2016-12-11 01:46:23 +0100

    Changed the plugin to use download_urls instead of ffmpeg

--- src/you_get/extractors/yizhibo.py
+++ src/you_get/extractors/yizhibo.py
@@ -9,7 +9,7 @@ import time
 def yizhibo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
     video_id = url[url.rfind('/')+1:].split(".")[0]
     json_request_url = 'http://www.yizhibo.com/live/h5api/get_basic_live_info?scid={}'.format(video_id)
-    content = get_html(json_request_url)
+    content = get_content(json_request_url)
     error = json.loads(content)['result']
     if (error != 1):
         raise ValueError("Error : {}".format(error))
@@ -18,11 +18,17 @@ def yizhibo_download(url, output_dir = '.', merge = True, info_only = False, **k
     title = data.get('data')['live_title']
     if (title == ''):
         title = data.get('data')['nickname']
-    real_url = data.get('data')['play_url']
-
-    print_info(site_info, title, 'flv', float('inf'))
+    m3u8_url = data.get('data')['play_url']
+    m3u8 = get_content(m3u8_url)
+    base_url = "/".join(data.get('data')['play_url'].split("/")[:7])+"/"
+    part_url = re.findall(r'([0-9]+\.ts)', m3u8)
+    real_url = []
+    for i in part_url:
+        url = base_url + i
+        real_url.append(url)
+    print_info(site_info, title, 'ts', float('inf'))
     if not info_only:
-        download_url_ffmpeg(real_url, title, 'flv', None, output_dir, merge = merge)
+        download_urls(real_url, title, 'ts', float('inf'), output_dir, merge = merge)
 
 site_info = "yizhibo.com"
 download = yizhibo_download
