fc2c77effaae54970e40246a1ceded8bcced6dc5
Valdemar Erk <valdemarerk@gmail.com>
2017-01-04 13:56:32 +0100

    Fixes quanmin, when stream is offline.

--- src/you_get/extractors/quanmin.py
+++ src/you_get/extractors/quanmin.py
@@ -9,11 +9,13 @@ import time
 def quanmin_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
     roomid = url[url.rfind("/")+1:]
     json_request_url = 'http://www.quanmin.tv/json/rooms/{}/info4.json'.format(roomid)
-
     content = get_html(json_request_url)
     data = json.loads(content)
 
     title = data["title"]
+    
+    if not data["play_status"]:
+        raise ValueError("The live stream is not online!")
     real_url = "http://flv.quanmin.tv/live/{}.flv".format(roomid)
 
     print_info(site_info, title, 'flv', float('inf'))
