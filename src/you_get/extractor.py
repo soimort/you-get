b06d7b496e61f7129263f8af9cc5d9b18106193d
ivan <qw7692336@gmail.com>
2017-02-18 17:01:51 +0800

    fixed bug

--- src/you_get/extractor.py
+++ src/you_get/extractor.py
@@ -194,12 +194,12 @@ class VideoExtractor():
                 urls = self.streams[stream_id]['src']
                 ext = self.streams[stream_id]['container']
                 total_size = self.streams[stream_id]['size']
-                segments = self.streams[stream_id]['segments'] if 'segments' in self.streams[stream_id] else segments
+                segments = self.streams[stream_id]['segments'] if 'segments' in self.streams[stream_id] else 1
             else:
                 urls = self.dash_streams[stream_id]['src']
                 ext = self.dash_streams[stream_id]['container']
                 total_size = self.dash_streams[stream_id]['size']
-                segments = self.streams[stream_id]['segments'] if 'segments' in self.streams[stream_id] else segments
+                segments = self.streams[stream_id]['segments'] if 'segments' in self.streams[stream_id] else 1
 
             if not urls:
                 log.wtf('[Failed] Cannot extract video source.')
