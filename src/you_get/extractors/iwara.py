12a842dd887e3bfb0ae27c1df771d3003ad3703f
w48325832 <w48325832@gmail.com>
2017-06-26 23:59:47 +0800

    add iwara support

--- src/you_get/extractors/iwara.py
+++ src/you_get/extractors/iwara.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 __all__ = ['iwara_download']
 from ..common import *
-from ..common import print_more_compatible as print
-from ..extractor import VideoExtractor
-from ..util import log
-from .. import json_output
 headers = {
     'DNT': '1',
     'Accept-Encoding': 'gzip, deflate, sdch, br',
@@ -13,28 +9,26 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Cache-Control': 'max-age=0',
-    'Referer': 'http://www.dilidili.com/',
+
     'Connection': 'keep-alive',
     'Save-Data': 'on',
+    'Cookie':'has_js=1;show_adult=1',
 }
-
-
 
 def iwara_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
     global headers
     video_hash=match1(url, r'http://ecchi.iwara.tv/videos/(\w+)')
-    html = get_html(url)
+    html = get_content(url,headers=headers)
     title = r1(r'<title>(.*)</title>', html)
     api_url='http://ecchi.iwara.tv/api/video/'+video_hash
-    content=get_html(api_url)
-    mime='video/mp4'
-    size=0;
-    print_info(content,title,mime,size)
+    content=get_content(api_url,headers=headers)
+    data=json.loads(content)
+    type,ext,size=url_info(data[0]['uri'], headers=headers)
+    down_urls=data[0]['uri']
+    print_info(down_urls,title+data[0]['resolution'],type,size)
 
-    print_info(site_info, title, mime, size)
-    if not info_only:
-        download_urls([source], title, ext, size, output_dir, merge=merge)
+    download_urls([down_urls], title, ext, size, output_dir, merge = merge,headers=headers)
 
 site_info = "iwara"
 download = iwara_download
-download_playlist = playlist_not_supported('archive')
+download_playlist = playlist_not_supported('iwara')
