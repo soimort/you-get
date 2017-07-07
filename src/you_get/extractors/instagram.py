72eb5cc34d5786ffb584fe18d25a4549c55de471
Mort Yao <soi@mort.ninja>
2017-07-08 01:54:49 +0200

    [instagram] support multiple image posts

--- src/you_get/extractors/instagram.py
+++ src/you_get/extractors/instagram.py
@@ -10,19 +10,43 @@ def instagram_download(url, output_dir='.', merge=True, info_only=False, **kwarg
     vid = r1(r'instagram.com/p/([^/]+)', url)
     description = r1(r'<meta property="og:title" content="([^"]*)"', html)
     title = "{} [{}]".format(description.replace("\n", " "), vid)
-
     stream = r1(r'<meta property="og:video" content="([^"]*)"', html)
     if stream:
         _, ext, size = url_info(stream)
-    else:
-        image = r1(r'<meta property="og:image" content="([^"]*)"', html)
-        ext = 'jpg'
-        _, _, size = url_info(image)
 
-    print_info(site_info, title, ext, size)
-    url = stream if stream else image
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
+        print_info(site_info, title, ext, size)
+        if not info_only:
+            download_urls([stream], title, ext, size, output_dir, merge=merge)
+    else:
+        data = re.search(r'window\._sharedData\s*=\s*(.*);</script>', html)
+        info = json.loads(data.group(1))
+
+        if 'edge_sidecar_to_children' in info['entry_data']['PostPage'][0]['graphql']['shortcode_media']:
+            edges = info['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']
+            for edge in edges:
+                title = edge['node']['shortcode']
+                image_url = edge['node']['display_url']
+                ext = image_url.split('.')[-1]
+                size = int(get_head(image_url)['Content-Length'])
+                print_info(site_info, title, ext, size)
+                if not info_only:
+                    download_urls(urls=[image_url],
+                                  title=title,
+                                  ext=ext,
+                                  total_size=size,
+                                  output_dir='.')
+        else:
+            title = info['entry_data']['PostPage'][0]['graphql']['shortcode_media']['shortcode']
+            image_url = info['entry_data']['PostPage'][0]['graphql']['shortcode_media']['display_url']
+            ext = image_url.split('.')[-1]
+            size = int(get_head(image_url)['Content-Length'])
+            print_info(site_info, title, ext, size)
+            if not info_only:
+                download_urls(urls=[image_url],
+                              title=title,
+                              ext=ext,
+                              total_size=size,
+                              output_dir='.')
 
 site_info = "Instagram.com"
 download = instagram_download
