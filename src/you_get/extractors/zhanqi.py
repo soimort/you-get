1ab8ea015de66af75d090b0fd24403e80052122c
MaxwellGoblin <lispmachine@outlook.com>
2017-07-15 21:44:54 +0800

    [common zhanqi]m3u8 code in common; rewrite zhanqi

--- src/you_get/extractors/zhanqi.py
+++ src/you_get/extractors/zhanqi.py
@@ -4,53 +4,48 @@ __all__ = ['zhanqi_download']
 
 from ..common import *
 import json
+import base64
+from urllib.parse import urlparse
 
 def zhanqi_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    host_name = url.split('/')[2]
-    first_folder_path = url.split('/')[3].split('?')[0]
+    path = urlparse(url).path[1:]
 
-    if first_folder_path != 'videos': #url = "https://www.zhanqi.tv/huashan?param_s=1_0.2.0"
-        if first_folder_path == 'topic': #https://www.zhanqi.tv/topic/lyingman
-            first_folder_path = url.split('/')[4].split('?')[0]
-        api_url = "https://www.zhanqi.tv/api/static/v2.1/room/domain/" + first_folder_path + ".json"
-        api_json = json.loads(get_html(api_url))
-        data = api_json['data']
-        status = data['status']
-        if status != '4':
-            raise ValueError ("The live stream is not online!")
-
-        nickname = data['nickname']
-        title = nickname + ": " + data['title']
-
-        roomid = data['id']
-        videoId = data['videoId']
-        jump_url = "http://wshdl.load.cdn.zhanqi.tv/zqlive/" + videoId + ".flv?get_url=1"
-        jump_url = jump_url.strip('\r\n')
-
-        real_url = get_html(jump_url)
-        real_url = real_url.strip('\r\n')
-        site_info = "www.zhanqi.tv"
-
-        print_info(site_info, title, 'flv', float('inf'))
-        if not info_only:
-            download_url_ffmpeg(real_url, title, 'flv', {}, output_dir = output_dir, merge = merge)
-
+    if not path.startswith('videos'): #url = "https://www.zhanqi.tv/huashan?param_s=1_0.2.0"
+        path_list = path.split('/')
+        room_id = path_list[1] if path_list[0] == 'topic' else path_list[0]
+        zhanqi_live(room_id, merge=merge, output_dir=output_dir, info_only=info_only, **kwargs)
     else: #url = 'https://www.zhanqi.tv/videos/Lyingman/2017/01/182308.html'
-        video_id = url.split('/')[-1].split('?')[0].split('.')[0]
-        assert video_id
-        api_url = "https://www.zhanqi.tv/api/static/v2.1/video/" + video_id + ".json"
-        api_json = json.loads(get_html(api_url))
-        data = api_json['data']
+        video_id = path.split('.')[0].split('/')[-1]
+        zhanqi_video(video_id, merge=merge, output_dir=output_dir, info_only=info_only, **kwargs)
 
-        title = data['title']
+def zhanqi_live(room_id, merge=True, output_dir='.', info_only=False, **kwargs):
+    api_url = "https://www.zhanqi.tv/api/static/v2.1/room/domain/{}.json".format(room_id)
+    json_data = json.loads(get_content(api_url))['data']
+    status = json_data['status']
+    if status != '4':
+        raise Exception("The live stream is not online!")
 
-        video_url_id = data['flashvars']['VideoID']
-        real_url = "http://dlvod.cdn.zhanqi.tv/" + video_url_id
-        site_info = "www.zhanqi.tv/videos"
+    nickname = json_data['nickname']
+    title = nickname + ": " + json_data['title']
+    video_levels = base64.b64decode(json_data['flashvars']['VideoLevels']).decode('utf8')
+    m3u8_url = json.loads(video_levels)['streamUrl']
 
-        print_info(site_info, title, 'flv', float('inf'))
-        if not info_only:
-            download_url_ffmpeg(real_url, title, 'flv', {}, output_dir = output_dir, merge = merge)
+    print_info(site_info, title, 'm3u8', 0, m3u8_url=m3u8_url, m3u8_type='master')
+    if not info_only:
+        download_url_ffmpeg(m3u8_url, title, 'mp4', output_dir=output_dir, merge=merge)
 
+def zhanqi_video(video_id, output_dir='.', info_only=False, merge=True, **kwargs):
+    api_url = 'https://www.zhanqi.tv/api/static/v2.1/video/{}.json'.format(video_id)
+    json_data = json.loads(get_content(api_url))['data']
+
+    title = json_data['title']
+    vid = json_data['flashvars']['VideoID']
+    m3u8_url = 'http://dlvod.cdn.zhanqi.tv/' + vid
+    urls = general_m3u8_extractor(m3u8_url)
+    print_info(site_info, title, 'm3u8', 0)
+    if not info_only:
+        download_urls(urls, title, 'ts', 0, output_dir=output_dir, merge=merge, **kwargs)
+
+site_info = "www.zhanqi.tv"
 download = zhanqi_download
 download_playlist = playlist_not_supported('zhanqi')
