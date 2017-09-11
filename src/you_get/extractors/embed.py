a534c11e947be993dd8e44a30cb83467376ade96
Mort Yao <soi@mort.ninja>
2017-09-11 13:23:02 +0200

    [embed] add dailymotion patterns

--- src/you_get/extractors/embed.py
+++ src/you_get/extractors/embed.py
@@ -5,6 +5,7 @@ import urllib.parse
 from ..common import *
 
 from .bilibili import bilibili_download
+from .dailymotion import dailymotion_download
 from .iqiyi import iqiyi_download_by_vid
 from .le import letvcloud_download_by_vu
 from .netease import netease_download
@@ -46,6 +47,8 @@ iqiyi_embed_patterns = [ 'player\.video\.qiyi\.com/([^/]+)/[^/]+/[^/]+/[^/]+\.sw
 netease_embed_patterns = [ '(http://\w+\.163\.com/movie/[^\'"]+)' ]
 
 vimeo_embed_patters = [ 'player\.vimeo\.com/video/(\d+)' ]
+
+dailymotion_embed_patterns = [ 'www\.dailymotion\.com/embed/video/(\w+)' ]
 
 """
 check the share button on http://www.bilibili.com/video/av5079467/
@@ -98,6 +101,11 @@ def embed_download(url, output_dir = '.', merge = True, info_only = False ,**kwa
     for url in urls:
         found = True
         vimeo_download_by_id(url, title=title, output_dir=output_dir, merge=merge, info_only=info_only, referer=url)
+
+    urls = matchall(content, dailymotion_embed_patterns)
+    for url in urls:
+        found = True
+        dailymotion_download(url, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
 
     aids = matchall(content, bilibili_embed_patterns)
     for aid in aids:
