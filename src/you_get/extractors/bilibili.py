5019d9d23fac0995c33f7b2ca5818840ed3a2156
Mort Yao <soi@mort.ninja>
2017-09-20 22:50:37 +0200

    [bilibili] quick hack for #2377 (enforce timeout 1)

--- src/you_get/extractors/bilibili.py
+++ src/you_get/extractors/bilibili.py
@@ -25,7 +25,7 @@ class Bilibili(VideoExtractor):
     live_api = 'http://live.bilibili.com/api/playurl?cid={}&otype=json'
     api_url = 'http://interface.bilibili.com/playurl?'
     bangumi_api_url = 'http://bangumi.bilibili.com/player/web_api/playurl?'
-    
+
     SEC1 = '1c15888dc316e05a15fdd0a02ed6584f'
     SEC2 = '9b288147e5474dd2aa67085f716c560d'
     stream_types = [
@@ -104,6 +104,8 @@ class Bilibili(VideoExtractor):
                 self.parse_bili_xml(api_xml)
 
     def prepare(self, **kwargs):
+        socket.setdefaulttimeout(1) # fail fast, very speedy!
+
         self.ua = fake_headers['User-Agent']
         self.url = url_locations([self.url])[0]
         frag = urllib.parse.urlparse(self.url).fragment
