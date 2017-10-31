fbd492220e3455db28c9389e4844d58a7967be8e
Mort Yao <soi@mort.ninja>
2017-10-31 19:02:44 +0100

    [tumblr] ignore bad URLs

--- src/you_get/extractors/tumblr.py
+++ src/you_get/extractors/tumblr.py
@@ -32,15 +32,17 @@ def tumblr_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
             tumblr_id = r1(r'^tumblr_(.+)_\d+$', title)
             quality = int(r1(r'^tumblr_.+_(\d+)$', title))
             ext = filename.split('.')[-1]
-            size = int(get_head(url)['Content-Length'])
-            if tumblr_id not in tuggles or tuggles[tumblr_id]['quality'] < quality:
-                tuggles[tumblr_id] = {
-                    'title': title,
-                    'url': url,
-                    'quality': quality,
-                    'ext': ext,
-                    'size': size,
-                }
+            try:
+                size = int(get_head(url)['Content-Length'])
+                if tumblr_id not in tuggles or tuggles[tumblr_id]['quality'] < quality:
+                    tuggles[tumblr_id] = {
+                        'title': title,
+                        'url': url,
+                        'quality': quality,
+                        'ext': ext,
+                        'size': size,
+                    }
+            except: pass
 
         if tuggles:
             size = sum([tuggles[t]['size'] for t in tuggles])
