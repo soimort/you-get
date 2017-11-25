4f7ba5aac8965b55fa0733f74d9a7cfb5a414ae3
Mort Yao <soi@mort.ninja>
2017-11-25 13:21:49 +0100

    [youku] update ccode

--- src/you_get/extractors/youku.py
+++ src/you_get/extractors/youku.py
@@ -78,7 +78,7 @@ class Youku(VideoExtractor):
         self.api_error_code = None
         self.api_error_msg = None
 
-        self.ccode = '0401'
+        self.ccode = '0406'
         self.utid = None
 
     def youku_ups(self):
@@ -154,7 +154,7 @@ class Youku(VideoExtractor):
                     log.wtf('Cannot fetch vid')
 
         if kwargs.get('src') and kwargs['src'] == 'tudou':
-            self.ccode = '050F'
+            self.ccode = '0406'
 
         if kwargs.get('password') and kwargs['password']:
             self.password_protected = True
