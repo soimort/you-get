344502af0d223def2a9ed0fde3766f6f7490b23b
Mort Yao <soi@mort.ninja>
2018-03-15 22:38:23 +0100

    [youku] resolve conflict

--- src/you_get/extractors/youku.py
+++ src/you_get/extractors/youku.py
@@ -78,7 +78,10 @@ class Youku(VideoExtractor):
         self.api_error_code = None
         self.api_error_msg = None
 
-        self.ccode = '0507'
+        if cookies:
+            self.ccode = '0513'
+        else:
+            self.ccode = '0507'
         self.utid = None
 
     def youku_ups(self):
