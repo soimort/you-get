ead0979ca133e75b62835c3d3ac2783955534a0f
Mort Yao <soi@mort.ninja>
2018-04-12 03:19:12 +0200

    [universal] relative path lah

--- src/you_get/extractors/universal.py
+++ src/you_get/extractors/universal.py
@@ -67,6 +67,14 @@ def universal_download(url, output_dir='.', merge=True, info_only=False, **kwarg
         urls += re.findall(r'href="(https?://[^"]+\.png)"', page, re.I)
         urls += re.findall(r'href="(https?://[^"]+\.gif)"', page, re.I)
 
+        # relative path
+        rel_urls = []
+        rel_urls += re.findall(r'href="(\.[^"]+\.jpe?g)"', page, re.I)
+        rel_urls += re.findall(r'href="(\.[^"]+\.png)"', page, re.I)
+        rel_urls += re.findall(r'href="(\.[^"]+\.gif)"', page, re.I)
+        for rel_url in rel_urls:
+            urls += [ r1(r'(.*/)', url) + rel_url ]
+
         # MPEG-DASH MPD
         mpd_urls = re.findall(r'src="(https?://[^"]+\.mpd)"', page)
         for mpd_url in mpd_urls:
