25aa2ac2e5f5b408edfc53f64a6706a716f0e0c3
Mort Yao <soi@mort.ninja>
2018-04-15 16:34:15 +0200

    [universal] better extraction of title and ext

--- src/you_get/extractors/universal.py
+++ src/you_get/extractors/universal.py
@@ -112,10 +112,9 @@ def universal_download(url, output_dir='.', merge=True, info_only=False, **kwarg
 
     else:
         # direct download
-        filename = parse.unquote(url.split('/')[-1])
-        title = '.'.join(filename.split('.')[:-1])
-        ext = filename.split('.')[-1]
-        _, _, size = url_info(url, faker=True)
+        filename = parse.unquote(url.split('/')[-1]) or parse.unquote(url.split('/')[-2])
+        title = '.'.join(filename.split('.')[:-1]) or filename
+        _, ext, size = url_info(url, faker=True)
         print_info(site_info, title, ext, size)
         if not info_only:
             download_urls([url], title, ext, size,
