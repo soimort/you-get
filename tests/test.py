ba3fccba7f069d14b7645e323958479ec6db6161
ooooo999 <ar33.msn@hotmail.com>
2019-02-26 11:24:28 +0800

    New test case
    Test case for downloading all videos from bilibili channel, where the extractor only output the latest 100 videos when there's more.

--- tests/test.py
+++ tests/test.py
@@ -3,33 +3,15 @@
 import unittest
 
 from you_get.extractors import (
-    imgur,
-    magisto,
-    youtube,
     bilibili,
 )
 
 
-class YouGetTests(unittest.TestCase):
-    def test_imgur(self):
-        imgur.download('http://imgur.com/WVLk5nD', info_only=True)
-        imgur.download('http://imgur.com/gallery/WVLk5nD', info_only=True)
+class YouGetTests(unittest.TestCase):	
+    def test_bilibili(self):
 
-    def test_magisto(self):
-        magisto.download(
-            'http://www.magisto.com/album/video/f3x9AAQORAkfDnIFDA',
-            info_only=True
-        )
-
-    def test_youtube(self):
-        youtube.download(
-            'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
-        )
-        youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
-        youtube.download(
-            'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
-            info_only=True
-        )
+        bilibili.download('https://space.bilibili.com/19111402/video', info_only=True)
+        youtube.download('https://space.bilibili.com/19111402/video', info_only=True)
 
 if __name__ == '__main__':
     unittest.main()
