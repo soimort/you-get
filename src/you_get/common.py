d961f7f49e511316b670f443be22aad97ec71968
Mort Yao <soi@mort.ninja>
2019-03-03 17:30:48 +0100

    [common] load_cookies(): load lines with prefix "#HttpOnly_" as MozillaCookieJar treats them as comments

--- src/you_get/common.py
+++ src/you_get/common.py
@@ -1276,10 +1276,68 @@ def download_main(download, download_playlist, urls, playlist, **kwargs):
 
 
 def load_cookies(cookiefile):
+    from http.cookiejar import Cookie
     global cookies
     try:
-        cookies = cookiejar.MozillaCookieJar(cookiefile)
-        cookies.load()
+        # MozillaCookieJar treats prefix '#HttpOnly_' as comments incorrectly!
+        # do not use its load()
+        # see also:
+        #   - https://docs.python.org/3/library/http.cookiejar.html#http.cookiejar.MozillaCookieJar
+        #   - https://github.com/python/cpython/blob/4b219ce/Lib/http/cookiejar.py#L2014
+        #   - https://curl.haxx.se/libcurl/c/CURLOPT_COOKIELIST.html#EXAMPLE
+        #cookies = cookiejar.MozillaCookieJar(cookiefile)
+        #cookies.load()
+        cookies = cookiejar.MozillaCookieJar()
+        now = time.time()
+        ignore_discard, ignore_expires = False, False
+        with open(cookiefile, 'r') as f:
+            for line in f:
+                # last field may be absent, so keep any trailing tab
+                if line.endswith("\n"): line = line[:-1]
+
+                # skip comments and blank lines XXX what is $ for?
+                if (line.strip().startswith(("#", "$")) or
+                    line.strip() == ""):
+                    if not line.strip().startswith('#HttpOnly_'):  # skip for #HttpOnly_
+                        continue
+
+                domain, domain_specified, path, secure, expires, name, value = \
+                        line.split("\t")
+                secure = (secure == "TRUE")
+                domain_specified = (domain_specified == "TRUE")
+                if name == "":
+                    # cookies.txt regards 'Set-Cookie: foo' as a cookie
+                    # with no name, whereas http.cookiejar regards it as a
+                    # cookie with no value.
+                    name = value
+                    value = None
+
+                initial_dot = domain.startswith(".")
+                if not line.strip().startswith('#HttpOnly_'):  # skip for #HttpOnly_
+                    assert domain_specified == initial_dot
+
+                discard = False
+                if expires == "":
+                    expires = None
+                    discard = True
+
+                # assume path_specified is false
+                c = Cookie(0, name, value,
+                           None, False,
+                           domain, domain_specified, initial_dot,
+                           path, False,
+                           secure,
+                           expires,
+                           discard,
+                           None,
+                           None,
+                           {})
+                if not ignore_discard and c.discard:
+                    continue
+                if not ignore_expires and c.is_expired(now):
+                    continue
+                cookies.set_cookie(c)
+
     except Exception:
         import sqlite3
         cookies = cookiejar.MozillaCookieJar()
