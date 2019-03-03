37dd620fc1e1e6517955c91d24eb7a68b306f86b
Mort Yao <soi@mort.ninja>
2019-03-03 17:40:54 +0100

    [youtube] load necessary cookies into headers (for age-restricted videos)

--- src/you_get/extractors/youtube.py
+++ src/you_get/extractors/youtube.py
@@ -195,7 +195,24 @@ class YouTube(VideoExtractor):
 
         elif video_info['status'] == ['fail']:
             if video_info['errorcode'] == ['150']:
-                video_page = get_content('https://www.youtube.com/watch?v=%s' % self.vid)
+                if cookies:
+                    # Load necessary cookies into headers (for age-restricted videos)
+                    consent, ssid, hsid, sid = 'YES', '', '', ''
+                    for cookie in cookies:
+                        if cookie.domain.endswith('.youtube.com'):
+                            if cookie.name == 'SSID':
+                                ssid = cookie.value
+                            elif cookie.name == 'HSID':
+                                hsid = cookie.value
+                            elif cookie.name == 'SID':
+                                sid = cookie.value
+                    cookie_str = 'CONSENT=%s; SSID=%s; HSID=%s; SID=%s' % (consent, ssid, hsid, sid)
+
+                    video_page = get_content('https://www.youtube.com/watch?v=%s' % self.vid,
+                                             headers={'Cookie': cookie_str})
+                else:
+                    video_page = get_content('https://www.youtube.com/watch?v=%s' % self.vid)
+
                 try:
                     ytplayer_config = json.loads(re.search('ytplayer.config\s*=\s*([^\n]+});ytplayer', video_page).group(1))
                 except:
