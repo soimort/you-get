7d90c6c776531861705a6e860485a63a695a8c74
wlkz <wikizhu@outlook.com>
2019-06-11 18:23:55 +0800

    change test for youtube 403

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+  
 import unittest
 
 from you_get.extractors import (
@@ -12,24 +12,9 @@ from you_get.extractors import (
 
 
 class YouGetTests(unittest.TestCase):
-    def test_imgur(self):
-        imgur.download('http://imgur.com/WVLk5nD', info_only=True)
-        imgur.download('http://imgur.com/gallery/WVLk5nD', info_only=True)
-
-    def test_magisto(self):
-        magisto.download(
-            'http://www.magisto.com/album/video/f3x9AAQORAkfDnIFDA',
-            info_only=True
-        )
-
     def test_youtube(self):
         youtube.download(
-            'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
-        )
-        youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
-        youtube.download(
-            'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
-            info_only=True
+            'https://www.youtube.com/watch?v=yS2rcvR6tFI', info_only=True
         )
 
 
