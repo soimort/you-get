fa05aa0b783825c07193963f82c8c1bb8fc23074
rjys365 <rjys365@hotmail.com>
2019-07-10 22:27:33 +0800

    测试获取一集仅大会员的番剧

--- tests/test.py
+++ tests/test.py
@@ -12,26 +12,8 @@ from you_get.extractors import (
 
 
 class YouGetTests(unittest.TestCase):
-    def test_imgur(self):
-        imgur.download('http://imgur.com/WVLk5nD', info_only=True)
-        imgur.download('http://imgur.com/gallery/WVLk5nD', info_only=True)
-
-    def test_magisto(self):
-        magisto.download(
-            'http://www.magisto.com/album/video/f3x9AAQORAkfDnIFDA',
-            info_only=True
-        )
-
-    def test_youtube(self):
-        youtube.download(
-            'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
-        )
-        youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
-        youtube.download(
-            'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
-            info_only=True
-        )
-
+    def test_bilibili(self):
+        bilibili.download('https://www.bilibili.com/bangumi/play/ep259653/',info_only=True)#未加入cookie，但即使加入cookie效果也一样
 
 if __name__ == '__main__':
     unittest.main()
