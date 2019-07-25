d3d397178fb2b24a7d0aabf11a995e293c968ebf
Mort Yao <soi@mort.ninja>
2019-07-25 23:08:23 +0800

    [common] update UA

--- src/you_get/common.py
+++ src/you_get/common.py
@@ -144,7 +144,7 @@ fake_headers = {
     'Accept-Charset': 'UTF-8,*;q=0.5',
     'Accept-Encoding': 'gzip,deflate,sdch',
     'Accept-Language': 'en-US,en;q=0.8',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:60.0) Gecko/20100101 Firefox/60.0',  # noqa
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0',  # noqa
 }
 
 if sys.stdout.isatty():
