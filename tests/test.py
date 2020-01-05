aaae162664e3addf11769cc53656eb4d8a9110f9
kpwd <jzmplusonesecond@outlook.com>
2020-01-05 19:07:46 +0800

    Can't download multipart video in Bilibili
    tetsai@tetsai-NH50-70RA:/tmp/pyou-get https://www.bilibili.com/video/av8977143 --playlist --debug
    [DEBUG] get_content: https://www.bilibili.com/video/av8977143
    [DEBUG] get_content: https://www.bilibili.com/video/av8977143
    [DEBUG] get_content: https://interface.bilibili.com/v2/playurl?appkey=iVGUTjsxvpLeuDCf&cid=14820815&otype=json&qn=80&quality=80&type=&sign=11864b9d7bf9c690fc70a58ffb7309a8
    [DEBUG] get_content: https://interface.bilibili.com/v2/playurl?appkey=iVGUTjsxvpLeuDCf&cid=14820815&otype=json&qn=64&quality=64&type=&sign=8077207c25f79596c169bc4602706f74
    [DEBUG] get_content: https://interface.bilibili.com/v2/playurl?appkey=iVGUTjsxvpLeuDCf&cid=14820815&otype=json&qn=32&quality=32&type=&sign=2a735c0e8729f87a13b460318e9d2573
    [DEBUG] get_content: https://interface.bilibili.com/v2/playurl?appkey=iVGUTjsxvpLeuDCf&cid=14820815&otype=json&qn=16&quality=16&type=&sign=d9cc6fb02041a8dc8bc7264e93b3ab58
    [DEBUG] get_content: http://comment.bilibili.com/14820815.xml
    you-get: version 0.4.1388, a tiny downloader that scrapes the web.
    you-get: Namespace(URL=['https://www.bilibili.com/video/av8977143'], auto_rename=False, cookies=None, debug=True, extractor_proxy=None, force=False, format=None, help=False, http_proxy=None, info=False, input_file=None, insecure=False, itag=None, json=False, no_caption=False, no_merge=False, no_proxy=False, output_dir='.', output_filename=None, password=None, player=None, playlist=True, skip_existing_file_size_check=False, socks_proxy=None, stream=None, timeout=600, url=False, version=False)
    Traceback (most recent call last):
      File "/home/tetsai/bin/you-get", line 11, in <module>
        you_get.main(repo_path=_filepath)
      File "/home/tetsai/Git/you-get/src/you_get/__main__.py", line 92, in main
        main(**kwargs)
      File "/home/tetsai/Git/you-get/src/you_get/common.py", line 1766, in main
        script_main(any_download, any_download_playlist, **kwargs)
      File "/home/tetsai/Git/you-get/src/you_get/common.py", line 1654, in script_main
        **extra
      File "/home/tetsai/Git/you-get/src/you_get/common.py", line 1308, in download_main
        download_playlist(url, **kwargs)
      File "/home/tetsai/Git/you-get/src/you_get/common.py", line 1762, in any_download_playlist
        m.download_playlist(url, **kwargs)
      File "/home/tetsai/Git/you-get/src/you_get/extractors/bilibili.py", line 632, in download_playlist_by_url
        self.download(**kwargs)
      File "/home/tetsai/Git/you-get/src/you_get/extractor.py", line 209, in download
        stream_id = itags[0]
    IndexError: list index out of range
    tetsai@tetsai-NH50-70RA:/tmp/p$

--- tests/test.py
+++ tests/test.py
@@ -3,44 +3,13 @@
 import unittest
 
 from you_get.extractors import (
-    imgur,
-    magisto,
-    youtube,
-    missevan,
-    acfun
+    bilibili,
 )
 
 
 class YouGetTests(unittest.TestCase):
-    def test_imgur(self):
-        imgur.download('http://imgur.com/WVLk5nD', info_only=True)
-        imgur.download('http://imgur.com/gallery/WVLk5nD', info_only=True)
-
-    def test_magisto(self):
-        magisto.download(
-            'http://www.magisto.com/album/video/f3x9AAQORAkfDnIFDA',
-            info_only=True
-        )
-
-    def test_missevan(self):
-        missevan.download('https://m.missevan.com/sound/1285995', info_only=True)
-        missevan.download_playlist(
-            'https://www.missevan.com/mdrama/drama/24130', info_only=True)
-        missevan.download_playlist(
-            'https://www.missevan.com/albuminfo/203090', info_only=True)
-
-    def test_youtube(self):
-        youtube.download(
-            'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
-        )
-        youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
-        youtube.download(
-            'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
-            info_only=True
-        )
-
-    def test_acfun(self):
-        acfun.download('https://www.acfun.cn/v/ac11701912', info_only=True)  
+        def test_bilibili(self):
+            bilibili.download('https://www.bilibili.com/video/av8977143', info_only=True)
 
 if __name__ == '__main__':
     unittest.main()
