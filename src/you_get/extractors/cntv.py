eca7a1d569d0d94b09aa4889fffccf18f6a1755e
jack-zheng <lanmolei0124@163.com>
2020-05-05 23:45:20 +0800

    support https type downloading from host of tv.cctv.com

--- src/you_get/extractors/cntv.py
+++ src/you_get/extractors/cntv.py
@@ -44,12 +44,12 @@ def cntv_download_by_id(rid, **kwargs):
 def cntv_download(url, **kwargs):
     if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
         rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
-    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
+    elif re.match(r'http(s)?://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
         rid = r1(r'var guid = "(\w+)"', get_content(url))
     elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
          re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
          re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
-         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
+         re.match(r'http(s)?://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
          re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
         page = get_content(url)
         rid = r1(r'videoCenterId","(\w+)"', page)
