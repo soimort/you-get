0bebd764fe5b545a51ce5bb2cb29dc9070c56da8
AOIDUO <zhouqi008012@gmail.com>
2020-09-21 00:31:56 +0800

    update test.py

--- tests/test.py
+++ tests/test.py
@@ -28,7 +28,7 @@ class YouGetTests(unittest.TestCase):
         youtube.download(
             'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
         )
-        youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
+        youtube.download('https://youtu.be/dswA84bw8I4', info_only=False)
         youtube.download(
             'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
             info_only=True
