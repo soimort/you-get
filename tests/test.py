db6ed38c6ad96caa23e98aaed5bad45f0fa437b3
Mort Yao <soi@mort.ninja>
2022-04-19 15:51:03 +0200

    [tests] enable test_tiktok

--- tests/test.py
+++ tests/test.py
@@ -53,10 +53,9 @@ class YouGetTests(unittest.TestCase):
         #    'https://soundcloud.com/anthony-flieger/sets/cytus', info_only=True
         #)
 
-    #def tests_tiktok(self):
-    #    tiktok.download('https://www.tiktok.com/@nmb48_official/video/6850796940293164290', info_only=True)
-    #    tiktok.download('https://t.tiktok.com/i18n/share/video/6850796940293164290/', info_only=True)
-    #    tiktok.download('https://vt.tiktok.com/UGJR4R/', info_only=True)
+    def test_tiktok(self):
+        tiktok.download('https://www.tiktok.com/@nmb48_official/video/6850796940293164290', info_only=True)
+        tiktok.download('https://t.tiktok.com/i18n/share/video/6850796940293164290/', info_only=True)
 
 
 if __name__ == '__main__':
