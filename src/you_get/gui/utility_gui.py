92035258acd63d7b5ab686f7fe41d3330c7d764a
veledara <71222580+veledara@users.noreply.github.com>
2023-04-23 19:08:04 +0300

    gui folder add

---
+++ src/you_get/gui/utility_gui.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("you-get")
+root.geometry("250x218")
+root.resizable(False, False)
+
+# Run the GUI
+root.mainloop()
+
+if __name__ == '__main__':
+    pass
