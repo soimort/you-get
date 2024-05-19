19a4f15d6014c66c99d48be4a024b89407fe428a
cerenkov <cerenkov@qq.com>
2024-05-19 12:03:54 +0800

    Revert "Update setup.py: compatibility for older python versions"
    This reverts commit 873ffdb61eb461b4c71ab7e12151864b49a23f3a.
    
    Minimun python version 3.7 already assures importlib availability

--- setup.py
+++ setup.py
@@ -5,21 +5,18 @@ PACKAGE_NAME = 'you_get'
 
 PROJ_METADATA = '%s.json' % PROJ_NAME
 
-import sys
-if (sys.version_info >= (3, 12)):
-    import importlib.util
-    import importlib.machinery
-    def load_source(modname, filename):
-        loader = importlib.machinery.SourceFileLoader(modname, filename)
-        spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
-        module = importlib.util.module_from_spec(spec)
-        # The module is always executed and not cached in sys.modules.
-        # Uncomment the following line to cache the module.
-        # sys.modules[module.__name__] = module
-        loader.exec_module(module)
-        return module
-else:
-    from imp import load_source
+import importlib.util
+import importlib.machinery
+
+def load_source(modname, filename):
+    loader = importlib.machinery.SourceFileLoader(modname, filename)
+    spec = importlib.util.spec_from_file_location(modname, filename, loader=loader)
+    module = importlib.util.module_from_spec(spec)
+    # The module is always executed and not cached in sys.modules.
+    # Uncomment the following line to cache the module.
+    # sys.modules[module.__name__] = module
+    loader.exec_module(module)
+    return module
 
 import os, json
 here = os.path.abspath(os.path.dirname(__file__))
