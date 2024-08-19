26db833b86e13f83acc00367533e07a35ab66e49
Mort Yao <soi@mort.ninja>
2024-08-19 18:41:21 +0200

    [tests] disable test_youtube temporarily

--- tests/test.py
+++ tests/test.py
@@ -27,18 +27,18 @@ class YouGetTests(unittest.TestCase):
             info_only=True
         )
 
-    def test_youtube(self):
-        youtube.download(
-            'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
-        )
+    #def test_youtube(self):
+        #youtube.download(
+        #    'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
+        #)
         #youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
         #youtube.download(
         #    'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
         #    info_only=True
         #)
-        youtube.download(
-            'https://www.youtube.com/watch?v=oRdxUFDoQe0', info_only=True
-        )
+        #youtube.download(
+        #    'https://www.youtube.com/watch?v=oRdxUFDoQe0', info_only=True
+        #)
 
     def test_acfun(self):
         acfun.download('https://www.acfun.cn/v/ac44560432', info_only=True)
