c0fdd58d7ebda2c1439f2f5e04dc8bd9173af5a6
Fuqiao Xue <xfq@w3.org>
2025-02-24 14:12:20 +0800

    YouTube failing

--- tests/test.py
+++ tests/test.py
@@ -27,10 +27,10 @@ class YouGetTests(unittest.TestCase):
             info_only=True
         )
 
-    #def test_youtube(self):
-        #youtube.download(
-        #    'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
-        #)
+    def test_youtube(self):
+        youtube.download(
+            'https://www.youtube.com/watch/?v\=LNEOx_Dkdzo', info_only=True
+        )
         #youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
         #youtube.download(
         #    'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
